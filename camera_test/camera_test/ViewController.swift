68e27897556bc2e757264e49bd34c4724db2cd49
John-ssj <Carry_John@users.noreply.github.com>
2020-08-06 16:44:25 +0800

    new commit

--- camera_test/camera_test/ViewController.swift
+++ camera_test/camera_test/ViewController.swift
@@ -11,17 +11,16 @@ import Foundation
 import AVFoundation
 
 class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
-    var subview: UIView!
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
     var photoOutput: AVCapturePhotoOutput!
-    
-//    var previewView: UIView!
+
+    var previewView = UIView()
     var captureImageView =  UIImageView()
     var takePicture = UIButton()
     var nextPicture = UIButton()
     
-    var i = 0
+    var picture_num = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,92 +30,60 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
     
     func setUpView() {
-        view.backgroundColor = UIColor.black
+        view.backgroundColor = UIColor.white
+        
+        view.addSubview(previewView)
+        previewView.backgroundColor = UIColor.black
+        previewView.translatesAutoresizingMaskIntoConstraints = false
+        previewView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        previewView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        previewView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        previewView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         
         view.addSubview(captureImageView)
         captureImageView.translatesAutoresizingMaskIntoConstraints = false
         captureImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         captureImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        captureImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        captureImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        captureImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        captureImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
         captureImageView.contentMode = .scaleAspectFill
         captureImageView.clipsToBounds = true
+        captureImageView.alpha = 0
         
         view.addSubview(takePicture)
-        takePicture.backgroundColor = .white
-        takePicture.layer.cornerRadius = 38
-        takePicture.layer.masksToBounds = true
+//        takePicture.backgroundColor = .white
+//        takePicture.layer.cornerRadius = 38
+//        takePicture.layer.masksToBounds = true
         takePicture.translatesAutoresizingMaskIntoConstraints = false
         takePicture.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        takePicture.widthAnchor.constraint(equalToConstant: 75).isActive = true
-        takePicture.topAnchor.constraint(equalTo: captureImageView.bottomAnchor, constant: 5).isActive = true
         takePicture.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
-        takePicture.setBackgroundImage(UIImage(systemName: "largecircle.fill.circle")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        takePicture.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        takePicture.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        takePicture.setBackgroundImage(UIImage(systemName: "largecircle.fill.circle")?.withRenderingMode(.alwaysOriginal).withTintColor(.white), for: .normal)
         takePicture.addTarget(self, action: #selector(TakePhoto), for: .touchDown)
         
         view.addSubview(nextPicture)
         nextPicture.translatesAutoresizingMaskIntoConstraints = false
-        nextPicture.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        nextPicture.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        nextPicture.topAnchor.constraint(equalTo: captureImageView.bottomAnchor, constant: 20).isActive = true
+        nextPicture.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
         nextPicture.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        nextPicture.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        nextPicture.heightAnchor.constraint(equalToConstant: 40).isActive = true
         nextPicture.setTitle("Next", for: .normal)
         nextPicture.titleLabel?.font = UIFont.systemFont(ofSize: 25)
+        nextPicture.setTitleColor(.white, for: .normal)
+        nextPicture.setTitleShadowColor(.black, for: .normal)
         nextPicture.addTarget(self, action: #selector(NextPhoto), for: .touchDown)
         nextPicture.alpha = 0
     }
     
-    
-    func setUpCamera() {
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .medium
-        guard let BackVideoCaptureDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else { print("No Back Camera"); return }
-        
-        
-        let videoInput: AVCaptureDeviceInput
-        do { videoInput = try AVCaptureDeviceInput(device: BackVideoCaptureDevice) } catch {
-            print("No Device Input")
-            return
-        }
-        if(captureSession.canAddInput(videoInput)){
-            captureSession.addInput(videoInput)
-        } else {
-            print("Could Not Add Input To Sesstion")
-            return
-        }
-        
-        
-        photoOutput = AVCapturePhotoOutput()
-        if (captureSession.canAddOutput(photoOutput)) {
-            captureSession.addOutput(photoOutput)
-        } else {
-            print("Could Not Add Output To Sesstion")
-            return
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.frame = captureImageView.layer.bounds
-        previewLayer.connection?.videoOrientation = .portrait
-        previewLayer.videoGravity = .resizeAspectFill
-        view.layer.addSublayer(previewLayer)
-        
-        captureSession.startRunning()
-        print("running")
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        setUpCamera()
-        
-        if (captureSession?.isRunning == false) {
+        if (captureSession.isRunning == false) {
             captureSession.startRunning()
             print("running")
         }
     }
-    
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -126,13 +93,41 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         }
     }
     
-    @objc func NextPhoto() {
-        if captureSession.isRunning { return }
+    func setUpCamera() {
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .high
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            photoOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(photoOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(photoOutput)
+                setupLivePreview()
+            }
+        } catch let error {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    func setupLivePreview() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(previewLayer)
         
-        captureSession.startRunning()
-        self.i += 1
         
-        nextPicture.alpha = 0
+        DispatchQueue.global(qos: .userInitiated).async { // [weak self] in
+            self.captureSession.startRunning()
+            
+            DispatchQueue.main.async {
+                self.previewLayer.frame = self.previewView.bounds
+            }
+        }
     }
     
     //拍照按钮
@@ -152,26 +147,38 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         captureSession.stopRunning()
     }
     
+    //next按钮
+    @objc func NextPhoto() {
+        if captureSession.isRunning { return }
+        
+        captureSession.startRunning()
+        self.picture_num += 1
+        
+        previewView.alpha = 1
+        captureImageView.alpha = 0
+        nextPicture.alpha = 0
+    }
+
     
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         guard let imageData = photo.fileDataRepresentation()
-        else {
-            print("error")
-            captureSession.startRunning()
-            return }
-
+            else {
+                captureSession.startRunning()
+                print("error")
+                return }
+        
         let image = UIImage(data: imageData)
         captureImageView.image = image
-        print("set image")
+        previewView.alpha = 0
+        captureImageView.alpha = 1
+        nextPicture.alpha = 1
+        
         if let imageData = image!.jpegData(compressionQuality: 1) as NSData? {
-            let fullPath = NSHomeDirectory().appending("/Documents/").appending("image_\(self.i)")
+            let fullPath = NSHomeDirectory().appending("/Documents/").appending("image_\(self.picture_num)")
             imageData.write(toFile: fullPath, atomically: true)
             print("fullPath=\(fullPath)")
         }
-        
-        nextPicture.alpha = 1
-        print("show next")
     }
-
+    
 }
 
